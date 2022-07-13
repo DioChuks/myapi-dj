1c087a5eb8d0706adf9b8c2fe6bd6f670cff33ed
Liam Dio <44742525+DioChuks@users.noreply.github.com>
2022-07-13 10:25:50 -0700

    login fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'myapi.apps.MyapiConfig',
 ]
 
@@ -119,3 +120,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
